a96ef068db9b335c0d33b1eeaae6815465e91259
jav <yakitin@hotmail.co.uk>
2018-08-09 10:13:19 +0100

    template added

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
 from HelloFlask import app
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello Flask!"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    return render_template(
+        "index.html", 
+        title = "Hello Flask",
+        message = "Hello, Flask!",
+        content = "on " + formatted_now)
