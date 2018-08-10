bb8b6e31467f1dfeb0851fd482529988340589d7
jav <yakitin@hotmail.co.uk>
2018-08-10 09:56:56 +0100

    Remainder of BasicProject tutorial

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -14,3 +14,14 @@ def home():
         title = "Hello Flask",
         message = "Hello, Flask!",
         content = "on " + formatted_now) 
+
+@app.route('/api/data')
+def get_data():
+    return app.send_static_file('data.json')
+
+@app.route('/about')
+def about():
+    return render_template(
+        "about.html",
+        title="About HelloFlask",
+        content = "Example app page for Flask.")
